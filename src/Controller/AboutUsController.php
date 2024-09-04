0f1ab9124eaa32cdaf8a177db89ebb59fd5f519f
Marta Gałecka <galecka_marta@wp.pl>
2024-09-04 12:57:34 +0200

    new commit

---
+++ src/Controller/AboutUsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class AboutUsController extends AbstractController
+{
+    #[Route('/about-us', name: 'about_us')]
+    public function index(): Response
+    {
+        return $this->render('about_us/index.html.twig');
+    }
+}
